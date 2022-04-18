ff595ab526cab9c07ea791aee957ea42945c0ef5
DaniV-ai <danikvv2@gmail.com>
2022-04-18 11:44:43 +0300

    Created class library projects and added it to main project. Created class User with SHA256 encrypting method.

---
+++ ClassLibGoreChat/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ClassLibGoreChat
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        User(string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+
+        public string GetSHA256FromPass()
+        {
+            byte[] hash;
+
+            if (Password != "")
+            {
+                SHA256 mySHA256 = SHA256.Create();
+                hash = mySHA256.ComputeHash(Password);
+
+                return hash.ToString();
+            }
+        }
+    }
+}
